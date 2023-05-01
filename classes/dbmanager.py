3663513cc006a02ec455ced901e4dab731bac71a
nvLast86 <n.lastenko@gmail.com>
2023-05-01 23:11:02 +0600

    Добавлены SQL запросы

--- classes/dbmanager.py
+++ classes/dbmanager.py
@@ -1,67 +1,49 @@
 import psycopg2
 
 
-class DBManager:
+class DBManager(DBEditor):
 
-    def create_database(database_name: str, params: dict) -> None:
-        conn = psycopg2.connect(dbname='postgres', **params)
-        conn.autocommit = True
-        cur = conn.cursor()
-
-        cur.execute(f"DROP DATABASE {database_name}")
-        cur.execute(f"CREATE DATABASE {database_name}")
-
-        conn.close()
-
-        conn = psycopg2.connect(dbname=database_name, **params)
-
-        with conn.cursor() as cur:
-            cur.execute("""
-                    CREATE TABLE employers (
-                        employer_id INTEGER PRIMARY KEY,
-                        name VARCHAR(255) NOT NULL,
-                        description TEXT
-                    )
-                """)
-
-        with conn.cursor() as cur:
-            cur.execute("""
-                    CREATE TABLE vacancies (
-                        vacancy_id SERIAL PRIMARY KEY,
-                        channel_id INT REFERENCES channels(channel_id),
-                        title VARCHAR NOT NULL,
-                        publish_date DATE,
-                        video_url TEXT
-                    )
-                """)
 
     def get_companies_and_vacancies_count(self):
         """
         Функция для получения списка всех компаний и количества вакансий у каждой компании
         """
+        sql_request = """SELECT employer_name, COUNT(vacancies.vacancy_id) FROM employers
+                         INNER JOIN vacancies USING (employer_id)
+                         GROUP BY employer_name"""
 
     def get_all_vacancies(self):
         """
         Функция получения списка всех вакансий с указанием названия компании, названия вакансии
         и зарплаты и ссылки на вакансию
         """
+        sql_request = """SELECT vacancy_name, employers.employer_name, salary_from, salary_to, 
+                         vacancy_link FROM vacancies
+                         INNER JOIN employers USING (employer_id)"""
 
     def get_avg_salary(self):
         """
         Функция получения средней зарплаты по вакансиям
         """
-
+        sql_request = """SELECT employers.employer_name, ROUND(AVG (salary_from)) as avg_salary_from, 
+                         ROUND(AVG(salary_to)) as avg_salary_to FROM vacancies
+                         INNER JOIN employers USING (employer_id)
+                         GROUP BY employers.employer_name"""
     def get_vacancies_with_higher_salary(self):
         """
         Функция получения списка всех вакансий, у которых зарплата выше средней по всем вакансиям
         """
+        sql_request = """SELECT vacancy_name, salary_from FROM vacancies
+                         WHERE salary_from > (SELECT AVG(salary_from) FROM vacancies)"""
 
     def get_vacancies_with_keyword(self):
         """
         Функция получения списка всех вакансий, в названии которых содержатся
         переданные в метод слова, например “python”
         """
-
+        sql_request = """SELECT vacancy_name, employers.employer_name FROM vacancies
+                         INNER JOIN employers USING (employer_id)
+                         WHERE vacancy_name LIKE '%python%'"""
 
 
 
