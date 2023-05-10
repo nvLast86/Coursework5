5c4ef270ebcba6b69dfeb939b6caa1ddd8853996
nvLast86 <n.lastenko@gmail.com>
2023-05-10 15:03:14 +0600

    В main.py добавлена функция main() для запуска программы.

--- main.py
+++ main.py
@@ -1,11 +1,45 @@
-employers = [{'id': 1740, 'name': 'Яндекс'},
-             {'id': 2180, 'name': 'OZON'},
-             {'id': 3529, 'name': 'СБЕР'},
-             {'id': 8550, 'name': 'Центр финансовых технологий'},
-             {'id': 39305, 'name': 'Газпромнефть'},
-             {'id': 41862, 'name': 'Контур'},
-             {'id': 64174, 'name': '2GIS'},
-             {'id': 67611, 'name': 'Тензор'},
-             {'id': 966315, 'name': 'АО Омский НИИ приборостроения'},
-             {'id': 2326492, 'name': 'Телеком-инжиниринг'}]
+from classes.headhunter import HeadHunter
+from classes.dbmanager import DBManager
+from utils.config import config
+
+
+def main():
+    # Создаем БД и выгружаем данные с сайта hh.ru
+    print("Добро пожаловать!")
+    print("Идет сборка данных, пожалуйста, подождите...")
+    print("Может занять длительное время!\n")
+    hh = HeadHunter()
+    params = config()
+    db = DBManager('headhunter', params)
+    db.create_database()
+    db.create_tables()
+    db.insert_data(hh.employers)
+    db.insert_data(hh.vacancies, False)
+
+    # Предлагаем пользователю поработать с БД
+    print(db)
+    print(f'На данный момент в базе данных {len(hh.vacancies)} вакансий.\n')
+    while True:
+        user_answer = input('Пожалуйста, введите номер действия (или "x" для выхода):\n')
+        if user_answer == '1':
+            print(db.get_companies_and_vacancies_count())
+        elif user_answer == '2':
+            print(db.get_all_vacancies())
+        elif user_answer == '3':
+            print(db.get_avg_salary())
+        elif user_answer == '4':
+            print(db.get_vacancies_with_higher_salary())
+        elif user_answer == '5':
+            user_keyword = input('Пожалуйста, введите ключевое слово:\n')
+            print(db.get_vacancies_with_keyword(user_keyword))
+        elif user_answer in ['x', 'X']:
+            break
+        else:
+            print("Неправильный ввод! Повторите пожалуйста")
+
+    print('Спасибо за работу!')
+
+
+if __name__ == '__main__':
+    main()
 
